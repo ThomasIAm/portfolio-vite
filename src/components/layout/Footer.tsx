d16168ef6246819bd4ab1a6eb60621aaa8b53d08
Thomas van den Nieuwenhoff <10333951+ThomasIAm@users.noreply.github.com>
2025-12-06 21:49:54 +0000

    Revert "Reverted to commit cde58890bb54a284a117316817fdbfde3c3d9f7c"
    This reverts commit 95ad106dfcde090a6843d811d4c25eb1845ff216.

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,10 +1,11 @@
-import { Github, Linkedin, Mail } from "lucide-react";
+import { Gitlab, Github, Linkedin, Mail } from "lucide-react";
 import { Link } from "react-router-dom";
 
 const socialLinks = [
-  { href: "https://linkedin.com/in/tvdn", icon: Linkedin, label: "LinkedIn" },
-  { href: "https://github.com/tvdn", icon: Github, label: "GitHub" },
-  { href: "mailto:hello@tvdn.me", icon: Mail, label: "Email" },
+  { href: "https://www.linkedin.com/in/tvdn", icon: Linkedin, label: "LinkedIn" },
+  { href: "https://github.com/ThomasIAm", icon: Github, label: "GitHub" },
+  { href: "https://gitlab.com/ThomasIAm", icon: Gitlab, label: "GitLab" },
+  { href: "mailto:thomas.vandennieuwenhoff@salt-security.com", icon: Mail, label: "Email" },
 ];
 
 export function Footer() {
