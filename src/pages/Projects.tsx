f157f165e54341446eb7cac61cbfdf6aee75d97f
Thomas van den Nieuwenhoff <10333951+ThomasIAm@users.noreply.github.com>
2026-06-29 22:09:15 +0200

    Update Projects.tsx

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -197,7 +197,7 @@ export default function Projects() {
               </Button>
               <Button variant="outline" size="lg" asChild>
                 <a
-                  href="https://gitlab.com/tvdn"
+                  href="https://gitlab.com/ThomasIAm"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
